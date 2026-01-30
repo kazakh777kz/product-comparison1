c73f456a978064763f6008312b48c0ed75a4474e
kazakh777kz <aleksandrovamir@gmail.com>
2026-01-30 14:11:55 +0500

    script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function filterTable() {
+  var select = document.getElementById("productSelect");
+  var value = select.value;
+  var table = document.getElementById("comparisonTable");
+  var rows = table.getElementsByTagName("tr");
+
+  for (var i = 1; i < rows.length; i++) {
+    var product = rows[i].cells[0].innerText;
+    if (product === value) {
+      rows[i].style.display = "";
+    } else {
+      rows[i].style.display = "none";
+    }
+  }
+}
